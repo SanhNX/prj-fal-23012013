af56ed464af758b28df966186db5dad90b572180
dtnghia88@gmail.com <dtnghia88@gmail.com@ec42fae9-f4ae-1d6c-652d-c070de006452>
2013-09-21 01:00:24 +0000

    demo export excel

--- trunk/Source/FalStore/FalStore/Control/DoanhThu.ascx.cs
+++ trunk/Source/FalStore/FalStore/Control/DoanhThu.ascx.cs
@@ -6,14 +6,15 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using BIZ;
 using Entity;
-
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Data;
 namespace FalStore.Control
 {
     public partial class DoanhThu : System.Web.UI.UserControl
     {
         BranchBIZ branBiz = new BranchBIZ();
         BillBIZ billBiz = new BillBIZ();
-
+       
         protected void Page_Load(object sender, EventArgs e)
         {
             InitPage();
@@ -47,6 +48,7 @@ namespace FalStore.Control
                 List<objDoanhThu> lstObj = new List<objDoanhThu>();
 
                 lstObj = billBiz.GetBillSearch(int.Parse(drpBranch.SelectedValue.ToString()), DateTime.Parse(txtStartDate.Text), DateTime.Parse(TxtEndDate.Text));
+ 
                 if (lstObj != null)
                 {
                     rptResult.DataSource = lstObj;
@@ -145,6 +147,88 @@ namespace FalStore.Control
 
         }
 
+        protected void btnExport_Click(object sender, EventArgs e)
+        {
+            List<objDoanhThu> lstObj = new List<objDoanhThu>();
 
+            lstObj = billBiz.GetBillSearch(int.Parse(drpBranch.SelectedValue.ToString()), DateTime.Parse(txtStartDate.Text), DateTime.Parse(TxtEndDate.Text));
+ 
+            DataTable tbl = new DataTable();
+            tbl.Columns.Add("MaHD", typeof(string));
+            tbl.Columns.Add("NhanVien", typeof(string));
+            tbl.Columns.Add("KhachHang", typeof(string));
+            tbl.Columns.Add("ChiNhanh", typeof(string));
+            tbl.Columns.Add("NgayLap", typeof(string));
+            tbl.Columns.Add("ThanhTien", typeof(string));
+            tbl.Columns.Add("GiamGia", typeof(string));
+            tbl.Columns.Add("TongTien", typeof(string));
+
+            DataRow dr;
+            foreach (var item in lstObj)
+            {
+                dr = tbl.NewRow();
+                dr["MaHD"] = item.BillID.ToString();
+                dr["NhanVien"] = item.EmployeeName.ToString();
+                dr["KhachHang"] = item.CustomerName.ToString();
+                dr["ChiNhanh"] = item.BranchName.ToString();
+                dr["NgayLap"] = item.CreateDate.ToLongDateString();
+                dr["ThanhTien"] = item.TotalPrice.ToString();
+                dr["GiamGia"] = item.Sale.ToString();
+                dr["TongTien"] = item.ActualTotalPrice.ToString();
+
+                tbl.Rows.Add(dr);
+            }
+
+            GenerateExcelFile(tbl, @"D:\demo123.xlsx");
+        }
+
+        public void GenerateExcelFile(DataTable dataTable, string directory)
+        {
+            Excel.Application application = new Excel.Application();
+            Excel.Workbook workbook = application.Workbooks.Add();
+            Excel.Worksheet worksheet = workbook.Sheets[1];
+
+            //DataTable dataTable = new DataTable();
+            //DataColumn column = new DataColumn("My Datacolumn");
+
+            //dataTable.Columns.Add(column);
+            //dataTable.Rows.Add(new object[] {"Foobar"});
+
+            var columns = dataTable.Columns.Count;
+            var rows = dataTable.Rows.Count;
+
+            Excel.Range range = worksheet.Range["A1", String.Format("{0}{1}", GetExcelColumnName(columns), rows)];
+
+            object[,] data = new object[rows,columns];
+
+            for (int rowNumber = 0; rowNumber < rows; rowNumber++)
+            {
+                for (int columnNumber = 0; columnNumber < columns; columnNumber++)
+                {
+                    data[rowNumber, columnNumber] = dataTable.Rows[rowNumber][columnNumber].ToString();
+                }
+            }
+
+            range.Value = data;
+
+            workbook.SaveAs(directory);
+            workbook.Close();    
+        }
+
+        private static string GetExcelColumnName(int columnNumber)
+        {
+            int dividend = columnNumber;
+            string columnName = String.Empty;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+
+            return columnName;
+        }
     }
 }
