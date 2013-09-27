e3b69e905acf2fe9542b9f5a5e31534d6aeca290
sanh23021987@gmail.com <sanh23021987@gmail.com@ec42fae9-f4ae-1d6c-652d-c070de006452>
2013-09-27 02:37:41 +0000

    add Entity.csproj

---
+++ trunk/Source/FalStore/Entity/objEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity
+{
+    public class objEvent
+    {
+        //ID
+        public int EventID { get; set; }
+        //Branch object
+        public objBranch Branch { get; set; }
+        //Name
+        public string EventName { get; set; }            
+        //Start date
+        public string StartDate { get; set; }
+        //End date
+        public string EndDate { get; set; }
+        //Discount
+        public string Discount { get; set; }
+
+    }
+}
