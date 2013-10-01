edcbfac03413f9d1410ca31cccc1822349623ada
sanh23021987@gmail.com <sanh23021987@gmail.com@ec42fae9-f4ae-1d6c-652d-c070de006452>
2013-10-01 01:55:57 +0000

    ban hang function

--- trunk/Source/FalStore/FalStore/sales.aspx.cs
+++ trunk/Source/FalStore/FalStore/sales.aspx.cs
@@ -6,6 +6,9 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Script.Serialization;
 using System.Web.Services;
+using BIZ;
+using DAL;
+using Entity;
 
 namespace FalStore
 {
@@ -13,76 +16,7 @@ namespace FalStore
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //Employee oEmployee1 =
-            //        new Employee { Name = "Pini", ID = "111", Age = "30" };
-
-            //Employee oEmployee2 =
-            //      new Employee { Name = "Yaniv", ID = "Cohen", Age = "31" };
-
-            //Employee oEmployee3 =
-            //      new Employee { Name = "Yoni", ID = "Biton", Age = "20" };
-
-            //List<Employee> oList = new List<Employee>() { oEmployee1, oEmployee2, oEmployee3 };
-
-            //if ((Request.Form != null) && (Request.Form.Count > 0))
-            //{
-            //    // Process HTTP POST Request
-            //    string strFilterID = Request.Form["ID"].ToString();
-            //    // List<clsContacts> lstContacts = getContactByID(strFilterID);
-            //    //Response.Write(buildXML(lstContacts));
-
-            //    JavaScriptSerializer oSerializer = new JavaScriptSerializer();
-            //    string sJSON = oSerializer.Serialize(oList);
-            //    Response.Write(sJSON);
-            //}
-            //else
-            //{
-            //    // Process HTTP GET Request
-            //    string strFilterID = Request.QueryString["ID"].ToString();
-            //    //
-            //    // List<clsContacts> lstContacts = getContactByID(strFilterID);
-            //    JavaScriptSerializer oSerializer = new JavaScriptSerializer();
-            //    string sJSON = oSerializer.Serialize(oList);
-            //    Response.Write(sJSON);
-            //}
-
-        }
-
-        protected void btnSave_Click(object sender, EventArgs e)
-        {
-            //Employee oEmployee1 =
-            //        new Employee { Name = "Pini", ID = "111", Age = "30" };
-
-            //Employee oEmployee2 =
-            //      new Employee { Name = "Yaniv", ID = "Cohen", Age = "31" };
-
-            //Employee oEmployee3 =
-            //      new Employee { Name = "Yoni", ID = "Biton", Age = "20" };
-
-            //List<Employee> oList = new List<Employee>() { oEmployee1, oEmployee2, oEmployee3 };
-
-            //if ((Request.Form != null) && (Request.Form.Count > 0))
-            //{
-            //    // Process HTTP POST Request
-            //    string strFilterID = Request.Form["ID"].ToString();
-            //   // List<clsContacts> lstContacts = getContactByID(strFilterID);
-            //    //Response.Write(buildXML(lstContacts));
-
-            //    JavaScriptSerializer oSerializer = new JavaScriptSerializer();
-            //    string sJSON = oSerializer.Serialize(oList);
-            //    Response.Write(sJSON);
-            //}
-            //else
-            //{
-            //    // Process HTTP GET Request
-            //    string strFilterID = Request.QueryString["ID"].ToString();
-            //    //
-            //   // List<clsContacts> lstContacts = getContactByID(strFilterID);
-            //    JavaScriptSerializer oSerializer = new JavaScriptSerializer();
-            //    string sJSON = oSerializer.Serialize(oList);
-            //    Response.Write(sJSON);
-            //}
-
+            
         }
 
         public class Employee
